bf5c423ec683ac853fb095942464c0860fdfa913
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2024-06-02 01:14:21 +0200

    switch between git accounts

--- config.fish
+++ config.fish
@@ -4,18 +4,51 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-starship init fish | source
-
-function nvm
-    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+# ----------------------------------------
+#          1. GIT USER MANAGEMENT
+# ----------------------------------------
+function log-git
+    set -l user_name (git config --global user.name)
+    set -l user_email (git config --global user.email)
+    echo "Current Git user.name: $user_name"
+    echo "Current Git user.email: $user_email"
 end
 
-# aliases / common commands
+# commands to switch between my git users confs
+function git-work
+    git config --global user.name "David 1"
+    git config --global user.email "email@company.com"
+    git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa_work"
+    echo "Switching to WORK git"
+    log-git
+end
+
+function git-github
+    git config --global user.name "David 2"
+    git config --global user.email "email@github.com"
+    git config --global core.sshCommand "ssh -i ~/.ssh/id_rsa_personal"
+    echo "Switching to GITHUB git"
+    log-git
+end
+
+# ----------------------------------------
+#          2. ALIASES
+# ----------------------------------------
+# git common commands
 alias ggf='git fetch'
-alais ggp='git pull'
+alias ggp='git pull'
 alias gfgp='git fetch && git pull'
+# clean branches, keeping master or develop
 alias git_clean_master='git branch | grep -v "master" | xargs git branch -D'
 alias git_clean_develop='git branch | grep -v "develop" | xargs git branch -D'
+
+
+# ----------------------------------------
+#          2. OTHER STUFF
+# ----------------------------------------
+
+# starship prompt
+starship init fish | source
 
 # pnpm
 set -gx PNPM_HOME "/Users/dawichi/Library/pnpm"
